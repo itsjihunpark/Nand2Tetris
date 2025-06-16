44626af3ce1205228f9bb77c670309989b3395bd
Jihun Park <jihunpark0989@gmail.com>
2025-06-16 19:22:46 +0100

    feat: project-4 assembly program added (machine language)

--- nand2tetris/projects/4/fill/Fill.asm
+++ nand2tetris/projects/4/fill/Fill.asm
@@ -9,3 +9,84 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+
+// check if key is pressed => if KBD =!0 then key is pressed, otherwised not pressed 
+// Starting at base address SCREEN, set 8192 word to -1 if a key is pressed, otherwise 0
+
+// R0 = 8192
+@8192
+D = A
+@R0
+M = D
+
+(CLEAR)
+    // i = 0
+    @i
+    M = 0
+    (CLEAR_LOOP)
+        // if (KBD != 0) goto FILL
+        @KBD
+        D=M
+        @FILL
+        D;JNE
+
+        // if (i>R0) goto CLEAR_LOOP
+        @i
+        D = M
+        @R0
+        D = M - D       
+        @CLEAR_LOOP
+        D;JEQ
+        // addr = SCREEN + i
+        @i
+        D = M
+        @SCREEN
+        A = D + A
+
+        // RAM[addr] = -1
+        M = 0
+
+        // i = i + 1
+        @i
+        M = M + 1
+        // goto CLEAR_LOOP
+        @CLEAR_LOOP
+        0;JMP
+
+(FILL)
+    // i = 0
+    @i
+    M = 0
+    (FILL_LOOP)
+        // if (KBD == 0) goto CLEAR
+        @KBD
+        D = M
+        @CLEAR
+        D;JEQ
+
+        // if (i>R0) goto FILL_LOOP
+        @i
+        D = M
+        @R0
+        D = M-D
+        @FILL_LOOP
+        D;JEQ
+        
+        // addr = SCREEN + i
+        @i
+        D = M
+        @SCREEN
+        A = D + A
+        
+        // RAM[addr] = 0
+        M = -1
+
+        // i = i + 1
+        @i
+        M = M + 1
+        
+        // goto FILL_LOOP
+        @FILL_LOOP
+        0;JMP
+
+        
--- nand2tetris/projects/4/mult/Mult.asm
+++ nand2tetris/projects/4/mult/Mult.asm
@@ -7,6 +7,40 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 // The algorithm is based on repetitive addition.
 
-//// Replace this comment with your code.
+// R2 = 0
+@R2
+M = 0
+// i = 0
+@i
+M = 0
+
+(LOOP)
+    //  if (i==R1) goto END
+    @i 
+    D = M
+    @R1
+    D = M-D
+    @END
+    D;JEQ
+
+    //  R2 =  R2 + R0
+    @R0
+    D = M
+    @R2
+    D = M + D
+    @R2
+    M = D
+    
+    //  i = i + 1
+    @i
+    M = M + 1
+
+    //  goto LOOP
+    @LOOP
+    0;JMP
+(END)
+    // goto END
+    @END
+    0;JMP
 
 
