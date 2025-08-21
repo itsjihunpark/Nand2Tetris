79f340ea5bc5c3029c5e4488938292689bc22a23
Jihun Park <jihunpark0989@gmail.com>
2025-08-21 12:44:24 +0100

    refactor: improved (maybe worsened) code...

--- nand2tetris/projects/7/VMTranslator/CodeWriter.py
+++ nand2tetris/projects/7/VMTranslator/CodeWriter.py
@@ -8,42 +8,40 @@ class CodeWriter:
             "that": "THAT",
         }
         self.arithmetic_asm_command_count = 0
-        
-        self.dest_file_name = argv[1].split("\\")[-1].replace(".vm","")
-        self.dest_file = open(f"{self.dest_file_name}.asm","w")
-        
-    def writeArithmetic(self, command:str):
         self.arithmetic_asm_commands = {
             "add":["//ADD","@13 // Y","D=M", "@14 // X","M=M+D",],
             "sub": ["//SUB","@13 // Y","D=M", "@14 // X","M=M-D",],
             "neg":["//NEG", "@14 // X", "M=-M",],
             "eq": ["//EQ","@13 // y","D=M", "@14 // x","D=M-D", 
-                   f"@EQ{self.arithmetic_asm_command_count}","D;JEQ","D=0",
-                   f"@ENDEQ{self.arithmetic_asm_command_count}","0;JMP",
-                   f"(EQ{self.arithmetic_asm_command_count})","D=-1",
-                   f"(ENDEQ{self.arithmetic_asm_command_count})","@14","M=D",
+                   f"@EQ$COUNTER","D;JEQ","D=0",
+                   f"@ENDEQ$COUNTER","0;JMP",
+                   f"(EQ$COUNTER)","D=-1",
+                   f"(ENDEQ$COUNTER)","@14","M=D",
             ],
             "gt":["//GT","@13  // Y","D=M", "@14 // X","D=M-D",
-                  f"@GT{self.arithmetic_asm_command_count}", "D;JGT","D=0",
-                  f"@ENDGT{self.arithmetic_asm_command_count}","0;JMP",
-                  f"(GT{self.arithmetic_asm_command_count})","D=-1",
-                  f"(ENDGT{self.arithmetic_asm_command_count})","@14","M=D",],
+                  f"@GT$COUNTER", "D;JGT","D=0",
+                  f"@ENDGT$COUNTER","0;JMP",
+                  f"(GT$COUNTER)","D=-1",
+                  f"(ENDGT$COUNTER)","@14","M=D",],
             "lt":["//LT","@13  // Y","D=M", "@14 // X","D=M-D",
-                  f"@LT{self.arithmetic_asm_command_count}", "D;JLT","D=0",
-                  f"@ENDLT{self.arithmetic_asm_command_count}","0;JMP",
-                  f"(LT{self.arithmetic_asm_command_count})","D=-1",
-                  f"(ENDLT{self.arithmetic_asm_command_count})","@14","M=D",],
+                  f"@LT$COUNTER", "D;JLT","D=0",
+                  f"@ENDLT$COUNTER","0;JMP",
+                  f"(LT$COUNTER)","D=-1",
+                  f"(ENDLT$COUNTER)","@14","M=D",],
             "and":["//AND","@13 // first pop value","D=M", 
                    "@14 // second pop value","M = M&D",],
             "or":["//OR","@13 // first pop value","D=M", 
                   "@14 // second pop value","M = M|D",],
             "not":["//NOT","@14 // first pop value","M=!M",]
         }
-
+        self.dest_file_name = argv[1].split("\\")[-1].replace(".vm","")
+        self.dest_file = open(f"{self.dest_file_name}.asm","w")
+        
+    def writeArithmetic(self, command:str):
         self.write_pop(0, 13)
         if command not in ["not", "neg"]:
             self.write_pop(0, 14)
-        self.dest_file.writelines(cmd+"\n" for cmd in self.arithmetic_asm_commands[command])
+        self.dest_file.writelines(cmd.replace("$COUNTER", str(self.arithmetic_asm_command_count))+"\n" for cmd in self.arithmetic_asm_commands[command])
         for cmd in self.arithmetic_asm_commands[command]:
             print(cmd)
         self.write_push(0, 14)
