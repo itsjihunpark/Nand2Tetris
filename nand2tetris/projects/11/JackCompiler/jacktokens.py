9a386d44313ee4ecf16e8faf6e394052092b3df1
Jihun Park <jihunpark0989@gmail.com>
2026-05-29 10:51:20 +0100

    debug: fixed compiler not handling comments correctly

--- nand2tetris/projects/11/JackCompiler/jacktokens.py
+++ nand2tetris/projects/11/JackCompiler/jacktokens.py
@@ -7,5 +7,5 @@ tokens = {
     "integerConstant":r"(?<![\'\"])\b[\d]+\b(?![\'\"])",
     "identifier":r"(?:\'[^\']*\'|\"[^\"]*\"|\b(?:class|constructor|function|method|field|static|var|int|char|boolean|void|true|false|null|this|let|do|if|else|while|return)\b)(*SKIP)(*F)|\b[a-zA-Z_]+[a-zA-Z0-9_]*\b",
 }
-comments = r"\/\/.*\n|\/\\*[\s\S]*?\*\/|\/\*\*[\s\S]*?\*\/"
+comments = r"\/\/.*\n|\/\*\*[\s\S]*?\*\/"
 
